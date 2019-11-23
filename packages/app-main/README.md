8c14f88676cda5990b70238c5df5e11757a0608d
Harry Gifford <harry@gifford.eu>
2019-11-22 18:12:59 -0800

    Update Rush/PNPM and related packages. Fix minor build issues.

--- packages/app-main/README.md
+++ packages/app-main/README.md
@@ -4,7 +4,16 @@ Bundles the app for the web and for local development.
 
 ## Commands
 
-| Command | Description |
-| --------| ------------|
-| `build` | Builds the bundle for use in production.
-| `start` | Creates a simple website that will update when changes to the code are made.
+| Command | Description                                                                  |
+| ------- | ---------------------------------------------------------------------------- |
+| `build` | Builds the bundle for use in production.                                     |
+| `start` | Creates a simple website that will update when changes to the code are made. |
+
+## Environment
+
+You can set the following commands to control the build.
+
+| Variable   | Type                       | Description                                                        |
+| ---------- | -------------------------- | ------------------------------------------------------------------ |
+| `NODE_ENV` | `production | development` | Set to `production` to generate an optimized build.                |
+| `HTTPS`    | `true | false`             | Set to true to run an HTTPS web server when running `rushx start`. |
--- packages/app-main/webpack.config.js
+++ packages/app-main/webpack.config.js
@@ -3,18 +3,21 @@
 const path = require("path");
 
 const isEnvDevelopment = process.env.NODE_ENV !== "production";
+const isHttps = process.env.HTTPS === "true";
 
 /** @type { import("webpack").Configuration } */
 module.exports = {
-    entry: "./src/index.tsx",
+    entry: {
+        bundle: "./src/index.tsx"
+    },
     devtool: isEnvDevelopment ? "eval-source-map" : "source-map",
     devServer: {
-        writeToDisk: true,
         open: true,
         contentBase: ["dist", "static"],
         publicPath: "dist",
         port: 3000,
-        overlay: true
+        overlay: true,
+        https: isHttps
     },
     mode: isEnvDevelopment ? "development" : "production",
     module: {
@@ -23,6 +26,13 @@ module.exports = {
                 test: /\.tsx?$/,
                 use: "ts-loader",
                 include: /src/
+            },
+            {
+                // Needed to load source maps for dependencies
+                // including files in `app-component`.
+                test: /\.jsx?$/,
+                use: ["source-map-loader"],
+                enforce: "pre"
             }
         ]
     },
@@ -30,7 +40,9 @@ module.exports = {
         extensions: [".tsx", ".ts", ".js", ".jsx", ".json"]
     },
     output: {
-        filename: "bundle.js",
-        path: path.resolve(__dirname, "dist")
+        filename: "[name].js",
+        chunkFilename: "[chunkhash].js",
+        path: path.resolve(__dirname, "dist"),
+        hashDigestLength: 5
     }
 };
